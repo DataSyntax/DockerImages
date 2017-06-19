15a48f47dbc92784a9f0b50c4e05c85df62a54cf
rivaros <r.ivantsiv@gmail.com>
2017-06-19 14:24:48 +0300

    added ubuntu-php70-npm image

--- rebuild.sh
+++ rebuild.sh
@@ -7,8 +7,10 @@ docker build -t datasyntax/ubuntu-base ubuntu-base/
 docker build -t datasyntax/ubuntu-mysql ubuntu-mysql/
 docker build -t datasyntax/ubuntu-php70 ubuntu-php70/
 docker build -t datasyntax/ubuntu-nginx ubuntu-nginx/
+docker build -t datasyntax/ubuntu-php70-npm ubuntu-php70-npm/
 
 docker push datasyntax/ubuntu-base
 docker push datasyntax/ubuntu-mysql
 docker push datasyntax/ubuntu-php70
 docker push datasyntax/ubuntu-nginx
+docker push datasyntax/ubuntu-php70-npm
